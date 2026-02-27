c9ae027e2d82d8d35672880e6d4c188f0faaf822
Shark-coder <taranm22@gmail.com>
2026-02-26 19:05:45 -0800

    This is the UI file
    I got pretty far, I didn't finish yet, mostly got it from my old program, will finish over the weekend. Just to clarify, this is not done.

---
+++ inventory.py
@@ -0,0 +1,31 @@
+from computer import Linux, Windows
+# from linked import LinkedComputer named your file this.
+
+
+def printMenu():
+    print("MENU")
+    print("L List all computers in your inventory")
+    print("A Add a computer")
+    print("R Remove some computers")
+    print("Q Quit")
+
+
+def readInt(prompt):
+    while True:
+        s = input(prompt).strip()
+        if s.isdigit():
+            return int(s)
+        print("No match. Please enter a whole number.\n")
+
+
+def readGB(prompt):
+    # accepts: "500", "500GB", "500 GB"
+    while True:
+        s = input(prompt).strip().lower().replace(" ", "")
+        if s.endswith("gb"):
+            s = s[:-2]
+        if s.isdigit():
+            gb = int(s)
+            if gb > 0:
+                return gb
+        print("No match. Enter a positive number like 500 GB.\n")
