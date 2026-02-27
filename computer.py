de4791b9e2b96ad06bf2d878d2ebba02ca57bad9
Shark-coder <taranm22@gmail.com>
2026-02-26 18:54:04 -0800

    Computer File
    I was working on the class part of it, feel free to add to it or tell me if anything is wrong, I feel i got pretty far

---
+++ computer.py
@@ -0,0 +1,38 @@
+class ComputerSystem:
+    def __init__(self, ip, year, os):
+        self.ip = ip
+        self.os = os
+        self.__yearPurchased = year  
+
+    def getYearPurchased(self):
+        return self.__yearPurchased
+
+    def getSpace(self):
+        raise NotImplementedError("getSpace() must be overridden")
+
+    def getStorageText(self):
+        return str(self.getSpace()) + "GB"
+
+
+class Linux(ComputerSystem):
+    def __init__(self, ip, year, os, fsGB):
+        super().__init__(ip, year, os)
+        self.fsGB = fsGB
+
+    def getSpace(self):
+        return self.fsGB
+
+    def getStorageText(self):
+        return "Filesystem: " + str(self.fsGB) + "GB"
+
+
+class Windows(ComputerSystem):
+    def __init__(self, ip, year, os, cGB):
+        super().__init__(ip, year, os)
+        self.cGB = cGB
+
+    def getSpace(self):
+        return self.cGB
+
+    def getStorageText(self):
+        return "C drive: " + str(self.cGB) + " GB"
